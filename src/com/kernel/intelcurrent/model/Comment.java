223c55680f62fc9cad7a4be2520067c885923611
allenjin <allen3jin@163.com>
2012-07-16 10:57:22 +0800

    上次少上传的类

---
+++ src/com/kernel/intelcurrent/model/Comment.java
@@ -0,0 +1,42 @@
+package com.kernel.intelcurrent.model;
+/**
+ * classname:Comment.java
+ * @author allenjin
+ * */
+public class Comment {
+	
+	private static final String TAG=Comment.class.getSimpleName();
+	
+	 /**
+     * 评论id
+     * */
+	public String id;
+	
+	 /**
+     * 评论者openid
+     * */
+	public String openid;
+	
+	 /**
+     * 评论者nick名称
+     * */
+	public String nick;
+	
+	 /**
+     * 评论时间戳
+     * */
+	public int timestamp;
+	
+	 /**
+     * 评论内容
+     * */
+	public String text;
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", openid=" + openid + ", 用户名=" + nick
+				+ ", timestamp=" + timestamp + ", 内容=" + text + "]";
+	}
+	
+	 
+}
---
+++ src/com/kernel/intelcurrent/model/ICArrayList.java
@@ -0,0 +1,14 @@
+package com.kernel.intelcurrent.model;
+
+import java.util.ArrayList;
+
+public class ICArrayList {
+	/**
+	 * 保存获取的结果并封装成list
+	 */
+	public ArrayList<?> list = new ArrayList();
+	/**
+	 * 保存获取的结果的标志位
+	 */
+	public boolean hasNext;
+}
